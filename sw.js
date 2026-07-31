c8136e19b6780c9d01d9533fa43fd7ed248b43e9
StumpHugelarge <hugelargestump@gmail.com>
2026-07-31 08:01:52 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "msc-cache-v4";
+const CACHE_NAME = "msc-cache-v5";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "./easter-egg.mp4"
 ];
 
 self.addEventListener("install", (event) => {
